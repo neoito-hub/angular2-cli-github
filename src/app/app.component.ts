631191e914d68830fba8761dc2bd5c1a3ec0e9d2
amalshehu <amalshehu@gmail.com>
2017-02-01 13:35:11 +0530

    Add methords

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from './github.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ export class AppComponent implements OnInit {
   title = 'Github Profile Search';
   githubUser: any;
   errorMessage: any;
-  constructor() {
+  constructor(private githubService: GithubService) {
     this.githubUser = {};
   }
   ngOnInit() {
@@ -17,7 +18,21 @@ export class AppComponent implements OnInit {
           this.getUserInformation();
   }
   searchUser() {
+      if (this.githubUser.userName && this.githubUser.userName.length > 0) {
+          this.githubService.updateUser(this.githubUser.userName);
+          this.getUserInformation();
+          // console.log(this.githubUser)
+      } else {
+          this.githubUser.user = false;
+      }
   }
 
   getUserInformation() {
+    this.githubService.getUser()
+        .subscribe(
+          data => this.githubUser.user = data,
+          error =>  this.errorMessage = <any>error
+        );
+        console.log('Github Username' + this.githubUser);
+  }
 }
