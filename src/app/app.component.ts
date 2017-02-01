16382043cb1fee70d120e27cdfd43913fcf0e0cd
amalshehu <amalshehu@gmail.com>
2017-02-01 13:32:16 +0530

    Add AppComponent class with onInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = 'Github Profile Search';
+  githubUser: any;
+  errorMessage: any;
+  constructor() {
+    this.githubUser = {};
+  }
+  ngOnInit() {
+          this.githubUser.user = false;
+          this.getUserInformation();
+  }
+  searchUser() {
+  }
+
+  getUserInformation() {
 }
